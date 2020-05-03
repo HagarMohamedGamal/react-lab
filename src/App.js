a672889181da966a448cb4747c84d2831c9ac803
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-05-03 02:18:57 +0200

    lab2 react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import SignUp from './components/authentication/SignUp';
+import SignIn from './components/authentication/SignIn';
+import TodoContainer from './components/todo/todoContainer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/signup">SignUp</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/signup">
+                <SignUp />
+              </Route>
+              <Route path="/login">
+                <SignIn />
+              </Route>
+              <Route path="/">
+                <TodoContainer />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+    );
+  }
 }
+
+
 
 export default App;
